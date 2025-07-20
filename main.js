3e0a2c3bc9e316d679b8d9e1dc61c9195e4d1d64
maksok49 <cukarskixmaks@mail.ru>
2025-07-21 01:51:38 +0500

    Update main.js

--- main.js
+++ main.js
@@ -5,14 +5,13 @@ function onTelegramAuth(user) {
 
 const script = document.createElement('script');
 script.src = 'https://telegram.org/js/telegram-widget.js?7';
-script.setAttribute('data-telegram-login', 'trueletnaya_bot'); // ← замени на имя своего бота
+script.setAttribute('data-telegram-login', 'trueletnaya_bot');
 script.setAttribute('data-size', 'large');
 script.setAttribute('data-userpic', 'false');
 script.setAttribute('data-request-access', 'write');
 script.setAttribute('data-onauth', 'onTelegramAuth(user)');
 document.body.appendChild(script);
 
-// Хранилище
 const flights = JSON.parse(localStorage.getItem('flights') || '[]');
 
 function renderFlights() {
